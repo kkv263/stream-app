3e216d2c397c26376877d20554eee33fd8d01e5b
Kevin Vu <kevin.vu@crometrics.com>
2022-07-30 18:15:27 -0500

    POTW-48: Implement logout

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,15 +1,23 @@
 import cookie from 'cookie';
 import type { Handle, GetSession } from "@sveltejs/kit";
 
+// https://kit.svelte.dev/docs/hooks
+
 export const handle:Handle = async({event, resolve}) => {
-  const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-
+  const cookies = Object.assign({user: null}, cookie.parse(event.request.headers.get('cookie') || ''));
   event.locals.user = cookies.user
-
   const response = await resolve(event);
 
-  // Because we redirect to homepage we set path to '/'
-  response.headers.set('set-cookie', `user=${event.locals.user || ''}; path=/; HttpOnly`);
+  const setCookies = {
+    'user': `user=${event.locals.user || ''}; path=/; HttpOnly`,
+    'token': `twttoken=${event.locals.token || ''}; path=/; HttpOnly`
+  }
+
+  // Set all necessary cookies depending on route
+  // This could mean setting them to null too
+  Object.values(setCookies).forEach(cookie => {
+    response.headers.append('set-cookie', cookie);
+  });
   return response;
 }
 
--- src/routes/api/v1/callback.ts
+++ src/routes/api/v1/callback.ts
@@ -5,43 +5,45 @@ import type { CallBackOptions } from '$lib/utils/types';
 
 const tokenURL = 'https://api.twitter.com/2/oauth2/token';
 const verifyObject = get(oauth_verifiers);
-const params: CallBackOptions = {
-  'client_id': import.meta.env.VITE_TWITTER_OAUTH_CLIENT_ID as string,
-  'redirect_uri' :'http://127.0.0.1:5173/api/v1/callback',
-  'grant_type': 'authorization_code',
-  'code_verifier': verifyObject.code_verifier,
-  'state': verifyObject.state,
-  'code': ''
-}
 
 export const GET: RequestHandler = async(event) => {
-    const state = event.url.searchParams.get('state');
-    params.code = event.url.searchParams.get('code')!;
-    // States don't match (vulnerable to attack)
-    if (state != params.state) {throw Error("States do not match, aborting");}
-    const token = await getToken();
-    const user = await getUser(token);
-    event.locals.user = user?.data.name
-    // Redirect to homepage for now. 
-    return {
-      status: 302,
-      headers: {
-        location: '/'
-      }
-    }
-};
+  const params: CallBackOptions = {
+    'client_id': import.meta.env.VITE_TWITTER_OAUTH_CLIENT_ID as string,
+    'redirect_uri' :'http://127.0.0.1:5173/api/v1/callback',
+    'grant_type': 'authorization_code',
+    'code_verifier': verifyObject.code_verifier,
+    'state': verifyObject.state,
+    'code': event.url.searchParams.get('code')!
+  }
 
-const getToken = async() => {
-  // One liner to transform json into application/x-www-form-urlencoded payload.
-  const formBody = Object.keys(params).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key as keyof CallBackOptions])).join('&');
-  return fetch(tokenURL, {
-    method: 'POST',
+  const getToken = async() => {
+    // One liner to transform json into application/x-www-form-urlencoded payload.
+    const formBody = Object.keys(params).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(params[key as keyof CallBackOptions])).join('&');
+    return fetch(tokenURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formBody
+    }).then(res => res.json())
+    .then(res => res.access_token);
+  };
+
+  const state = event.url.searchParams.get('state');
+  // States don't match (vulnerable to attack)
+  if (state != params.state) {throw Error("States do not match, aborting");}
+  // TODO: Refresh Token and rename events.local.user
+  const token = await getToken();
+  const user = await getUser(token);
+  event.locals.token = token;
+  event.locals.user = user?.data.name;
+  // Redirect to homepage for now. 
+  return {
+    status: 302,
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: formBody
-  }).then(res => res.json())
-  .then(res => res.access_token);
+      location: '/'
+    }
+  }
 };
 
 const getUser = async(token:string) => {
