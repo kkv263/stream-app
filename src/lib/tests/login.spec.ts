45f34451100ed3e4db68f84682579e6ef6f949b9
Kevin Vu <kevin.vu@crometrics.com>
2022-07-31 00:30:56 -0500

    comment out login test placeholder

--- src/lib/tests/login.spec.ts
+++ src/lib/tests/login.spec.ts
@@ -1,8 +1,8 @@
-const fetchMock = jest
-.spyOn(global, 'fetch')
-.mockImplementation( jest.fn(() => Promise.resolve({ 
-  json: () => Promise.resolve({ 
-    data: 100 
-  }), 
-}), ) as jest.Mock );
+// const fetchMock = jest
+// .spyOn(global, 'fetch')
+// .mockImplementation( jest.fn(() => Promise.resolve({ 
+//   json: () => Promise.resolve({ 
+//     data: 100 
+//   }), 
+// }), ) as jest.Mock );
   
