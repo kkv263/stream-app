2311d57c3cd4e1619d5853c4ed51b94bac723f38
Kevin Vu <kevin.vu@crometrics.com>
2022-07-30 20:16:08 -0500

    POTW-52: Update ENV variables

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -19,7 +19,7 @@ const verifier = cryptoRandomString({length:128, type:'alphanumeric'});
 const challenge = base64URLEncode(sha256(verifier));
 
 const params: RedirectOptions = {
-  'client_id': import.meta.env.VITE_TWITTER_OAUTH_CLIENT_ID as string,
+  'client_id': import.meta.env.DEV ? import.meta.env.VITE_TWITTER_OAUTH_CLIENT_ID as string : process.env.TWITTER_OAUTH_CLIENT_ID,
   'redirect_uri' :'http://127.0.0.1:5173/api/v1/callback',
   'scope': "tweet.read%20users.read%20tweet.write%20offline.access",
   'state': cryptoRandomString({length: 15, type: 'distinguishable'}),
