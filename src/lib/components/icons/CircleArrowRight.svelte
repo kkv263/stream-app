fc52b548449e56b6ee82b7ced70a29c838d5b0d0
Kevin Vu <kevin.vu@crometrics.com>
2022-08-03 22:36:51 -0500

    Add icons

---
+++ src/lib/components/icons/CircleArrowRight.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let width:string;
+  export let height:string;
+  export let marginLeft:string = '';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} style="margin-left:{marginLeft}" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
