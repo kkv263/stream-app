15caa80876d16360005ff78dd209d6c29a53491d
Kevin Vu <kevin.vu@crometrics.com>
2022-08-13 11:03:40 -0500

    twitter render tests

--- src/lib/tests/Twitter.spec.ts
+++ src/lib/tests/Twitter.spec.ts
@@ -1,9 +1,17 @@
 import { render, screen } from '@testing-library/svelte';
-import { expect, test } from 'vitest';
 import Twitter from "$lib/components/Twitter/Twitter.svelte";
 
-test("says 'hello world!'", () => {
-    render(Twitter);
-    const node = screen.queryByText("Hello world!");
+describe('twitter conditional render', () => {
+  it('should not render twitter content if user is not logged in', () => {
+    render(Twitter, {'twtUser': null});
+    const node = screen.getByText('You are not logged in');
     expect(node).not.toBeNull();
-})
+  });
+  it('should render twitter content if user is logged in', () => {
+    render(Twitter, {'twtUser': 'twitteruser'});
+    const node = screen.getByText('Welcome twitteruser');
+    expect(node).not.toBeNull();
+  });
+});
+
+// TODO: Twitter mocking tests.
