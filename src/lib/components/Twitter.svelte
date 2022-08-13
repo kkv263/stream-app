760a15bca76e342f7f00d0380c99b48843b625a5
Kevin Vu <kevin.vu@crometrics.com>
2022-08-13 09:27:53 -0500

    twitter error handle ish

--- src/lib/components/Twitter.svelte
+++ src/lib/components/Twitter.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
 	import Section from "$lib/components/layout/Section.svelte";
 
-	// TODO: Handle 403 Error: no duplicate messages.
+	let tweet:string;
 	const  handleClick = async() => {
 		try {
 			const data = await fetch('/api/v1/twitter/tweet', {
 				method: 'POST',
 				body: JSON.stringify({
-					text: 'testing'
+					text: tweet
 				})
 			}).then(res => res.json());
+
+			if (data.errors) {
+				console.log(data.errors);
+				return;
+			}
+
+			if (data.status === 403) {
+				// Create a modal for error
+				console.log(data.detail);
+			}
+
+			// Update with data
 			console.log(data);
+			alert('success');
 		}
 		catch (error) {
 			console.log(error);
@@ -24,7 +37,7 @@
   {#if twtUser}
     <h2>Welcome {twtUser}</h2>
     <a href='logout/twitter'>logout tweeter</a>
-		<input type="text">
+		<input type="text"  bind:value={tweet}>
 		<button on:click={handleClick}>Click me</button>
   {:else}
 		<h2>You are not logged in</h2>
