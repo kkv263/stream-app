1eaf9829d19a7eb2c02816e761548bde12c56800
Kevin Vu <kevin.vu@crometrics.com>
2022-08-17 21:40:09 -0500

    fix syntax and some build errors

--- src/lib/tests/Twitter.spec.ts
+++ src/lib/tests/Twitter.spec.ts
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/svelte';
-import Twitter from '$lib/components/Twitter/Twitter.svelte';
+import Twitter from '../components/Twitter/Twitter.svelte';
 
 describe('twitter conditional render', () => {
   it('should not render twitter content if user is not logged in', () => {
